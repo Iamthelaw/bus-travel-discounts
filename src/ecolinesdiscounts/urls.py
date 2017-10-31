ed442245cc1d082855e238b064956bffa49e59d9
Anton Alekseev <robotehnik@me.com>
2017-10-31 21:23:44 +0300

    Security issue: admin site url
    Now admin site url set-up going to be from env variable
    + Add env variable name to .env.default
    + Changed url logic

--- src/ecolinesdiscounts/urls.py
+++ src/ecolinesdiscounts/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import os
+
 from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('spa_app.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^%s/' % os.environ.get('ADMIN_URL', 'admin'), admin.site.urls),
     url(r'^auth/', include('djoser.urls')),
+    url(r'^auth/', include('djoser.urls.authtoken')),
+    url(r'^', include('spa_app.urls')),
 ]
