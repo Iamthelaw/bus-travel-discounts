4736d3acf2ca8f9f55d71471ec10a8e95e7f8564
Anton Alekseev <robotehnik@me.com>
2017-11-13 00:48:12 +0300

    Pylint cleanup + models updates

--- src/discount/admin.py
+++ src/discount/admin.py
@@ -1,4 +1,5 @@
 # encoding: utf-8
+"""Admin classes for discounts models"""
 from django.contrib import admin
 
 from discount.models import Currency, Discount
@@ -6,9 +7,11 @@ from discount.models import Currency, Discount
 
 @admin.register(Currency)
 class CurrencyAdmin(admin.ModelAdmin):
+    """Admin class for Currency model."""
     pass
 
 
 @admin.register(Discount)
 class DiscountAdmin(admin.ModelAdmin):
+    """Admin class for Discount model."""
     pass
--- src/discount/models.py
+++ src/discount/models.py
@@ -1,33 +1,12 @@
-# encoding: utf-8
+"""Model classes for discounts app."""
 from django.db import models
 
 from geo_data.models import City
 
 
-class TimelessManager(models.Manager):
-    def get_queryset(self):
-        return (
-            super(TimelessManager, self)
-                .get_queryset()
-                .filter(
-                    time_start__isnull=True,
-                    time_end__isnull=True
-                )
-        )
-
-
-class PricelessManager(models.Manager):
-    def get_queryset(self):
-        return (
-            super(PricelessManager, self)
-                .get_queryset()
-                .filter(price__lte=0)
-        )
-
-
 class Currency(models.Model):
     """Currency implementation."""
-    code = models.CharField(max_length=3)
+    code = models.CharField(max_length=3, unique=True)
     symbol = models.CharField(max_length=1)
 
 
@@ -36,12 +15,18 @@ class Discount(models.Model):
     from_city = models.ForeignKey(City, related_name='discount_from_city')
     to_city = models.ForeignKey(City, related_name='discount_to_city')
 
-    price = models.DecimalField(default=0.00, decimal_places=2, max_digits=8)
-    currency = models.ForeignKey(Currency)
+    original_price = models.DecimalField(
+        default=0.00, decimal_places=2, max_digits=8)
+    original_currency = models.ForeignKey(Currency)
 
     time_start = models.DateField(null=True, blank=True)
     time_end = models.DateField(null=True, blank=True)
 
-    objects = models.Manager()
-    timeless =  TimelessManager()
-    priceless = PricelessManager()
+    #: Who knows how long can be url string?
+    link = models.TextField(blank=True)
+
+    #: What parser created this record
+    parser = models.CharField(max_length=200, default='AnknownParser')
+
+    class Meta:
+        unique_together = (('from_city', 'to_city'), )
